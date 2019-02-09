c6aaede35c030f464293aaa43a11f98b914207e0
Forcedsquids <tktommykline@gmail.com>
2019-02-08 23:44:50 -0500

    First commit

---
+++ Four-unit-game.js
@@ -0,0 +1,7 @@
+'use strict'
+$(document).ready(function() {
+    var Winningnumber = function random_item() {
+        Math.floor[Math.random() * (120 - 19)];
+    console.log(Winningnumber)
+    }  
+});
