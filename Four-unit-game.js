288a9c1bebb0775c308406d4c3ef4d4679b376a6
Forcedsquids <tktommykline@gmail.com>
2019-02-14 16:07:32 -0500

    added code to gen random numbers

--- Four-unit-game.js
+++ Four-unit-game.js
@@ -1,7 +1,39 @@
-'use strict'
-$(document).ready(function() {
-    var Winningnumber = function random_item() {
-        Math.floor[Math.random() * (120 - 19)];
-    console.log(Winningnumber)
-    }  
+
+$( document ).ready(function() {
+// name the vars of the game
+    var winningNumber
+    var userNumber
+    var Button1
+    var Button2
+    var Button3
+    var Button4
+
+    var wins = 0
+    var losses = 0
+//make on click events for the buttons
+$('#Button-1').on("click", random_buttons);
+
+   //
+// have the computer select a random number between 19 - 120    
+    function random_item() {
+       winningNumber = Math.floor[Math.random() * 120] + 19;
+    console.log(winningNumber);
+    }; 
+// write something for the random numbers in the buttons
+    function random_buttons(event) {
+        Math.floor[Math.random() * 12];
+       console.log(event.target);
+    };
+// write something that adds the random numbers from the buttons on each click 
+        function button_numbers() {
+        
+        };
+// logic if the "userNumber" === "winningNumber" wins +1
+// logic if the "userNumber" > "winningNumber" losses +1
+    if (userNumber === winningNumber) {
+        wins++
+    } else if (userNumber > winningNumber) {
+        losses++
+    };
+// make sure winning or losing randomises all random numbers?????
 });
